6fdfc47f0ea25901fd7c4ae39035b3d8a15c303d
Jud4 <Jud4@users.noreply.github.com>
2022-12-14 23:06:35 -0400

    Request API with Search bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { useState } from "react";
 
 function App() {
   const getQuery = async ({query})=>{
-    console.log(query);
+    // Pre-processing query for API
+    query = query.replace(/ /g,'+');
+    const url = `https://itunes.apple.com/search?term=${query}&media=music&attribute=artistTerm&entity=album&limit=30`;
+    
+    //Request the API
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
   }
   return (
     <div className="App">
--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -4,29 +4,30 @@ const SearchComponent = ({ onGetQuery }) => {
     //set Hook state
     const [query, setQuery] = useState("");
     
-const onSubmit = (e) => {
-    e.preventDefault();
-    // Send Query to the principal Component
-    onGetQuery({query});
-    // Reset the search bar's text
-    setQuery("");
-};
+    const onSubmit = (e) => {
+        e.preventDefault();
+        // Send Query to the principal Component
+        onGetQuery({query});
+        // Reset the search bar's text
+        setQuery("");
+    };
 
 
-return (
-    <form onSubmit={onSubmit}>
-        <input
-            type="search"
-            placeholder="Search Album"
-            aria-label="Search"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-        />
-        <button type="submit">
-            Search
-        </button>
-    </form>
-)
+    return (
+        <form className="d-flex" onSubmit={onSubmit}>
+            <input
+                type="search"
+                className="form-control me-1.5"
+                placeholder="Search Album"
+                aria-label="Search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <button className="btn btn-outline-success m-1.5" type="submit">
+                Search
+            </button>
+        </form>
+    )
 };
 
 export default SearchComponent;
